763c85a0ee8c04e5b8446abfab1b536b6d88237b
Abhinav Marwaha <abhinavmarwaha913@gmail.com>
2022-05-12 02:08:40 -0500

    settingsStore-draft

--- app/src/main/java/com/abhinavmarwaha/walletx/di/ArchModelModule.kt
+++ app/src/main/java/com/abhinavmarwaha/walletx/di/ArchModelModule.kt
@@ -3,6 +3,7 @@ package com.abhinavmarwaha.walletx.di
 import com.abhinavmarwaha.walletx.archmodel.CardGroupsStore
 import com.abhinavmarwaha.walletx.archmodel.CardsStore
 import com.abhinavmarwaha.walletx.archmodel.Repository
+import com.abhinavmarwaha.walletx.archmodel.SettingsStore
 import org.kodein.di.DI
 import org.kodein.di.bind
 import org.kodein.di.singleton
