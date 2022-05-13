cdf388e2928d4318759da8dff5da39aa884e445b
Abhinav Marwaha <abhinavmarwaha913@gmail.com>
2022-05-13 16:12:18 -0500

    just image encryption not working

--- app/src/main/java/com/abhinavmarwaha/walletx/MainActivity.kt
+++ app/src/main/java/com/abhinavmarwaha/walletx/MainActivity.kt
@@ -13,8 +13,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.PreferenceDataStoreFactory
 import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.preferencesDataStore
 import androidx.navigation.NavController
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.compose.NavHost
@@ -23,7 +23,6 @@ import androidx.navigation.compose.rememberNavController
 import com.abhinavmarwaha.walletx.db.room.*
 import com.abhinavmarwaha.walletx.di.archModelModule
 import com.abhinavmarwaha.walletx.lock.LockedActivity
-import com.abhinavmarwaha.walletx.lock.PatternLockView
 import com.abhinavmarwaha.walletx.models.Money
 import com.abhinavmarwaha.walletx.ui.AddCardView
 import com.abhinavmarwaha.walletx.ui.AllCards
@@ -35,17 +34,40 @@ import com.abhinavmarwaha.walletx.ui.theme.DarkRed
 import com.abhinavmarwaha.walletx.ui.theme.WalletXTheme
 import com.abhinavmarwaha.walletx.ui.widgets.LongButton
 import com.abhinavmarwaha.walletx.ui.widgets.SmallButton
+import com.google.crypto.tink.Aead
+import com.google.crypto.tink.KeyTemplates
+import com.google.crypto.tink.aead.AeadConfig
+import com.google.crypto.tink.integration.android.AndroidKeysetManager
+import io.github.osipxd.datastore.encrypted.createEncrypted
 import org.kodein.di.*
+import java.io.File
 
 class MainActivity : ComponentActivity(), DIAware {
-    private val dataStore: DataStore<Preferences> by preferencesDataStore(name = "money")
+
 
     override val di by DI.lazy {
-        bind<AppDatabase>() with singleton { AppDatabase.getInstance(this@MainActivity) }
+        val context = this@MainActivity
+
+        bind<AppDatabase>() with singleton { AppDatabase.getInstance(context) }
         bind<CardDAO>() with singleton { instance<AppDatabase>().cardDao() }
         bind<CardGroupDAO>() with singleton { instance<AppDatabase>().cardGroupDao() }
         bind<CGRelationDao>() with singleton { instance<AppDatabase>().cgRelationDao() }
         bind<NotesDao>() with singleton { instance<AppDatabase>().notesDao() }
+
+        AeadConfig.register();
+
+        val aead = AndroidKeysetManager.Builder()
+            .withSharedPref(context, "master_keyset", "master_key_preference")
+            .withKeyTemplate(KeyTemplates.get("AES256_GCM"))
+            .withMasterKeyUri("android-keystore://master_key")
+            .build()
+            .keysetHandle
+            .getPrimitive(Aead::class.java)
+
+        val dataStore: DataStore<Preferences> = PreferenceDataStoreFactory.createEncrypted(aead) {
+            File(context.filesDir, "datastore.preferences_pb")
+        }
+
         bind<Money>() with singleton { Money(dataStore) }
 
         import(archModelModule)
--- app/src/main/java/com/abhinavmarwaha/walletx/crypto/datastoreCrypto.kt
+++ app/src/main/java/com/abhinavmarwaha/walletx/crypto/datastoreCrypto.kt
@@ -1,174 +1,173 @@
 package com.abhinavmarwaha.walletx.crypto
-
-import android.content.Context
-import android.os.Build
-import android.security.keystore.KeyGenParameterSpec
-import android.security.keystore.KeyProperties
-import androidx.annotation.RequiresApi
-import androidx.datastore.core.CorruptionException
-import androidx.datastore.core.DataStore
-import androidx.datastore.core.DataStoreFactory
-import androidx.datastore.core.Serializer
-import androidx.datastore.dataStoreFile
-import androidx.datastore.preferences.core.PreferenceDataStoreFactory
-import androidx.datastore.preferences.core.Preferences
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
-import java.io.File
-import java.io.IOException
-import java.io.InputStream
-import java.io.OutputStream
-import java.security.KeyStore
-import javax.crypto.Cipher
-import javax.crypto.KeyGenerator
-import javax.crypto.SecretKey
-import javax.crypto.spec.IvParameterSpec
-
-/*
-    Reference : https://stackoverflow.com/questions/65653650/androidx-datastore-aes-cbc-pkcs7-javax-crypto-illegalblocksizeexception
- */
-
-interface CipherProvider {
-    val encryptCipher: Cipher
-    fun decryptCipher(iv: ByteArray): Cipher
-}
-
-@RequiresApi(Build.VERSION_CODES.M)
-class AesCipherProvider constructor(
-    private val keyName: String,
-    private val keyStore: KeyStore,
-    private val keyStoreName: String
-) : CipherProvider {
-
-    override val encryptCipher: Cipher
-        get() = Cipher.getInstance(TRANSFORMATION).apply {
-            init(Cipher.ENCRYPT_MODE, getOrCreateKey())
-        }
-
-    override fun decryptCipher(iv: ByteArray): Cipher =
-        Cipher.getInstance(TRANSFORMATION).apply {
-            init(Cipher.DECRYPT_MODE, getOrCreateKey(), IvParameterSpec(iv))
-        }
-
-
-    private fun getOrCreateKey(): SecretKey =
-        (keyStore.getEntry(keyName, null) as? KeyStore.SecretKeyEntry)?.secretKey
-            ?: generateKey()
-
-    private fun generateKey(): SecretKey =
-        KeyGenerator.getInstance(ALGORITHM, keyStoreName)
-            .apply { init(keyGenParams) }
-            .generateKey()
-
-    private val keyGenParams =
-        KeyGenParameterSpec.Builder(
-            keyName,
-            KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT
-        ).apply {
-            setBlockModes(BLOCK_MODE)
-            setEncryptionPaddings(PADDING)
-            setUserAuthenticationRequired(false)
-            setRandomizedEncryptionRequired(true)
-        }.build()
-
-    companion object {
-        const val ALGORITHM = KeyProperties.KEY_ALGORITHM_AES
-        const val BLOCK_MODE = KeyProperties.BLOCK_MODE_CBC
-        const val PADDING = KeyProperties.ENCRYPTION_PADDING_PKCS7
-        const val TRANSFORMATION = "$ALGORITHM/$BLOCK_MODE/$PADDING"
-    }
-}
-
-interface Crypto {
-    fun encrypt(rawBytes: ByteArray, outputStream: OutputStream)
-    fun decrypt(inputStream: InputStream): ByteArray
-}
-
-class CryptoImpl constructor(private val cipherProvider: CipherProvider) : Crypto {
-
-    override fun encrypt(rawBytes: ByteArray, outputStream: OutputStream) {
-        val cipher = cipherProvider.encryptCipher
-        val encryptedBytes = cipher.doFinal(rawBytes)
-        with(outputStream) {
-            write(cipher.iv.size)
-            write(cipher.iv)
-            write(encryptedBytes.size)
-            write(encryptedBytes)
-        }
-    }
-
-    override fun decrypt(inputStream: InputStream): ByteArray {
-        val ivSize = inputStream.read()
-        val iv = ByteArray(ivSize)
-        inputStream.read(iv)
-        val encryptedDataSize = inputStream.read()
-        val encryptedData = ByteArray(encryptedDataSize)
-        inputStream.read(encryptedData)
-        val cipher = cipherProvider.decryptCipher(iv)
-        return cipher.doFinal(encryptedData)
-    }
-}
-
-object SecurityModule {
-
-    const val KEY_NAME = "Key Name"
-    const val KEY_STORE_NAME = "Key Store Name"
-
-    private const val ANDROID_KEY_STORE_TYPE = "AndroidKeyStore"
-    private const val SIMPLE_DATA_KEY_NAME = "SimpleDataKey"
-
-    fun provideKeyStore(): KeyStore =
-        KeyStore.getInstance(ANDROID_KEY_STORE_TYPE).apply { load(null) }
-
-    fun providesKeyName(): String = SIMPLE_DATA_KEY_NAME
-
-    fun providesKeyStoreName(): String = ANDROID_KEY_STORE_TYPE
-
-
-    interface Declarations {
-
-        fun bindsCipherProvider(impl: AesCipherProvider): CipherProvider
-
-        fun bindsCrypto(impl: CryptoImpl): Crypto
-    }
-}
-
-class SecureSimpleDataSerializer(private val crypto: Crypto) :
-    Serializer<Preferences> {
-
-    override fun readFrom(input: InputStream): Preferences {
-        return if (input.available() != 0) {
-            try {
-
-                Preferences.ADAPTER.decode(crypto.decrypt(input))
-            } catch (exception: IOException) {
-                throw CorruptionException("Cannot read proto", exception)
-            }
-        } else {
-            Preferences()
-        }
-    }
-
-    override fun writeTo(t: SimpleData, output: OutputStream) {
-        crypto.encrypt(SimpleData.ADAPTER.encode(t), output)
-    }
-}
-
-object DataStoreModule {
-
-    fun providesDataStore(
-        context: Context,
-        crypto: Crypto
-    ): DataStore<Preferences> =
-        DataStoreFactory.create(
-            serializer = SecureSimpleDataSerializer(crypto), // your Serializer
-            corruptionHandler = null,
-            migrations = emptyList(),
-            scope = CoroutineScope(Dispatchers.IO + Job())
-        )
-//        PreferenceDataStoreFactory.create()
-        {
-            File(context.filesDir, "DataStoreTest.pb")
-        }
-}
+//
+//import android.content.Context
+//import android.os.Build
+//import android.security.keystore.KeyGenParameterSpec
+//import android.security.keystore.KeyProperties
+//import androidx.annotation.RequiresApi
+//import androidx.datastore.core.CorruptionException
+//import androidx.datastore.core.DataStore
+//import androidx.datastore.core.DataStoreFactory
+//import androidx.datastore.core.Serializer
+//import androidx.datastore.dataStoreFile
+//import androidx.datastore.preferences.core.PreferenceDataStoreFactory
+//import androidx.datastore.preferences.core.Preferences
+//import androidx.datastore.preferences.preferencesDataStoreFile
+//import kotlinx.coroutines.CoroutineScope
+//import kotlinx.coroutines.Dispatchers
+//import kotlinx.coroutines.Job
+//import java.io.File
+//import java.io.IOException
+//import java.io.InputStream
+//import java.io.OutputStream
+//import java.security.KeyStore
+//import javax.crypto.Cipher
+//import javax.crypto.KeyGenerator
+//import javax.crypto.SecretKey
+//import javax.crypto.spec.IvParameterSpec
+//
+///*
+//    Reference : https://stackoverflow.com/questions/65653650/androidx-datastore-aes-cbc-pkcs7-javax-crypto-illegalblocksizeexception
+// */
+//
+//interface CipherProvider {
+//    val encryptCipher: Cipher
+//    fun decryptCipher(iv: ByteArray): Cipher
+//}
+//
+//@RequiresApi(Build.VERSION_CODES.M)
+//class AesCipherProvider constructor(
+//    private val keyName: String,
+//    private val keyStore: KeyStore,
+//    private val keyStoreName: String
+//) : CipherProvider {
+//
+//    override val encryptCipher: Cipher
+//        get() = Cipher.getInstance(TRANSFORMATION).apply {
+//            init(Cipher.ENCRYPT_MODE, getOrCreateKey())
+//        }
+//
+//    override fun decryptCipher(iv: ByteArray): Cipher =
+//        Cipher.getInstance(TRANSFORMATION).apply {
+//            init(Cipher.DECRYPT_MODE, getOrCreateKey(), IvParameterSpec(iv))
+//        }
+//
+//
+//    private fun getOrCreateKey(): SecretKey =
+//        (keyStore.getEntry(keyName, null) as? KeyStore.SecretKeyEntry)?.secretKey
+//            ?: generateKey()
+//
+//    private fun generateKey(): SecretKey =
+//        KeyGenerator.getInstance(ALGORITHM, keyStoreName)
+//            .apply { init(keyGenParams) }
+//            .generateKey()
+//
+//    private val keyGenParams =
+//        KeyGenParameterSpec.Builder(
+//            keyName,
+//            KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT
+//        ).apply {
+//            setBlockModes(BLOCK_MODE)
+//            setEncryptionPaddings(PADDING)
+//            setUserAuthenticationRequired(false)
+//            setRandomizedEncryptionRequired(true)
+//        }.build()
+//
+//    companion object {
+//        const val ALGORITHM = KeyProperties.KEY_ALGORITHM_AES
+//        const val BLOCK_MODE = KeyProperties.BLOCK_MODE_CBC
+//        const val PADDING = KeyProperties.ENCRYPTION_PADDING_PKCS7
+//        const val TRANSFORMATION = "$ALGORITHM/$BLOCK_MODE/$PADDING"
+//    }
+//}
+//
+//interface Crypto {
+//    fun encrypt(rawBytes: ByteArray, outputStream: OutputStream)
+//    fun decrypt(inputStream: InputStream): ByteArray
+//}
+//
+//class CryptoImpl constructor(private val cipherProvider: CipherProvider) : Crypto {
+//
+//    override fun encrypt(rawBytes: ByteArray, outputStream: OutputStream) {
+//        val cipher = cipherProvider.encryptCipher
+//        val encryptedBytes = cipher.doFinal(rawBytes)
+//        with(outputStream) {
+//            write(cipher.iv.size)
+//            write(cipher.iv)
+//            write(encryptedBytes.size)
+//            write(encryptedBytes)
+//        }
+//    }
+//
+//    override fun decrypt(inputStream: InputStream): ByteArray {
+//        val ivSize = inputStream.read()
+//        val iv = ByteArray(ivSize)
+//        inputStream.read(iv)
+//        val encryptedDataSize = inputStream.read()
+//        val encryptedData = ByteArray(encryptedDataSize)
+//        inputStream.read(encryptedData)
+//        val cipher = cipherProvider.decryptCipher(iv)
+//        return cipher.doFinal(encryptedData)
+//    }
+//}
+//
+//object SecurityModule {
+//
+//    const val KEY_NAME = "Key Name"
+//    const val KEY_STORE_NAME = "Key Store Name"
+//
+//    private const val ANDROID_KEY_STORE_TYPE = "AndroidKeyStore"
+//    private const val SIMPLE_DATA_KEY_NAME = "SimpleDataKey"
+//
+//    fun provideKeyStore(): KeyStore =
+//        KeyStore.getInstance(ANDROID_KEY_STORE_TYPE).apply { load(null) }
+//
+//    fun providesKeyName(): String = SIMPLE_DATA_KEY_NAME
+//
+//    fun providesKeyStoreName(): String = ANDROID_KEY_STORE_TYPE
+//
+//
+//    interface Declarations {
+//
+//        fun bindsCipherProvider(impl: AesCipherProvider): CipherProvider
+//
+//        fun bindsCrypto(impl: CryptoImpl): Crypto
+//    }
+//}
+//
+//class SecureSimpleDataSerializer(private val crypto: Crypto, private val context: Context) :
+//    Serializer<Preferences> {
+//
+//    override fun readFrom(input: InputStream): Preferences {
+//        return if (input.available() != 0) {
+//            try {
+//                Preferences.ADAPTER.decode(crypto.decrypt(input))
+//            } catch (exception: IOException) {
+//                throw CorruptionException("Cannot read proto", exception)
+//            }
+//        } else {
+//            PreferenceDataStoreFactory.crea
+//        }
+//    }
+//
+//    override fun writeTo(t: SimpleData, output: OutputStream) {
+//        crypto.encrypt(SimpleData.ADAPTER.encode(t), output)
+//    }
+//}
+//
+//object DataStoreModule {
+//    fun providesDataStore(
+//        context: Context,
+//        crypto: Crypto
+//    ): DataStore<Preferences> =
+//        DataStoreFactory.create(
+//            serializer = SecureSimpleDataSerializer(crypto), // your Serializer
+//            corruptionHandler = null,
+//            migrations = emptyList(),
+//            scope = CoroutineScope(Dispatchers.IO + Job())
+//        )
+////        PreferenceDataStoreFactory.create()
+//        {
+//            File(context.filesDir, "PrefDataStore.pb")
+//        }
+//}
