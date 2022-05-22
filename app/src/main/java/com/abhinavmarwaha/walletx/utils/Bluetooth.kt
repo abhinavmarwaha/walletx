0842687c4f48381014dcabebac9ad691632c0e6b
Abhinav Marwaha <abhinavmarwaha913@gmail.com>
2022-05-22 06:17:53 -0500

    ble

---
+++ app/src/main/java/com/abhinavmarwaha/walletx/utils/Bluetooth.kt
@@ -0,0 +1,40 @@
+package com.abhinavmarwaha.walletx.utils
+
+import android.bluetooth.le.ScanCallback
+import android.bluetooth.le.ScanResult
+import android.os.Handler
+
+class Bluetooth {
+    private val bluetoothLeScanner = bluetoothAdapter.bluetoothLeScanner
+    private var scanning = false
+    private val handler = Handler()
+
+    // Stops scanning after 10 seconds.
+    private val SCAN_PERIOD: Long = 10000
+
+    private fun scanLeDevice() {
+        if (!scanning) { // Stops scanning after a pre-defined scan period.
+            handler.postDelayed({
+                scanning = false
+                bluetoothLeScanner.stopScan(leScanCallback)
+            }, SCAN_PERIOD)
+            scanning = true
+            bluetoothLeScanner.startScan(leScanCallback)
+        } else {
+            scanning = false
+            bluetoothLeScanner.stopScan(leScanCallback)
+        }
+    }
+
+    private val leDeviceListAdapter = LeDeviceListAdapter()
+    // Device scan callback.
+    private val leScanCallback: ScanCallback = object : ScanCallback() {
+        override fun onScanResult(callbackType: Int, result: ScanResult) {
+            super.onScanResult(callbackType, result)
+            leDeviceListAdapter.addDevice(result.device)
+            leDeviceListAdapter.notifyDataSetChanged()
+        }
+    }
+
+
+}
